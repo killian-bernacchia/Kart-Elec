d047a2cf0141af96eb5f2161030d741adf442b84
killian-bernacchia <etude.killbern@gmail.com>
2025-02-03 02:56:22 +0100

    motor_control_task added

--- motor_control.c
+++ motor_control.c
@@ -5,7 +5,7 @@
 #define SAMPLES_SIZE 10
 #define PEDAL_CORRECTION_FACTOR 1.4f
 
-void ErrorNoSignal(Speed_Data speed_data);
+static void ErrorNoSignal(Speed_Data speed_data);
 
 Filter _speed_filter;
 uint16_t _speed_samples[SAMPLES_SIZE]; 
@@ -13,7 +13,7 @@ percent_t _speed_limite_ratio;
 bool motor_is_unsafe;
 
 QueueHandle_t user_speed_cmd_2_motor_ctrl_queue; // speed_command_tsk => motor_control_tsk
-QueueHandle_t user_speed_cmd_2_communication_queue; // speed_command_tsk => communication_tsk
+QueueHandle_t motor_ctrl_queue_cmd_2_communication_queue; // speed_command_tsk => communication_tsk
 
 void motor_control_init(percent_t speed_limite_ratio)
 {
@@ -31,7 +31,7 @@ void motor_control_init(percent_t speed_limite_ratio)
 
     motor_is_unsafe = false;
     user_speed_cmd_2_motor_ctrl_queue = xQueueCreate(1, sizeof(Speed_Data));
-    user_speed_cmd_2_communication_queue = xQueueCreate(1, sizeof(Speed_Data));
+    motor_ctrl_queue_cmd_2_communication_queue = xQueueCreate(1, sizeof(Speed_Data));
 }
 
 void motor_control_set_speed_limite_ratio(percent_t speed_limite_ratio)
@@ -42,12 +42,42 @@ void motor_control_set_speed_limite_ratio(percent_t speed_limite_ratio)
 void motor_control_reset();
 
 //send the speed to the motor
-void motor_control_task(void *pvParameters);
+void motor_control_task(void *pvParameters)
+{
+    Speed_Data speed_data;
+    Heat_Data heat_data;
+
+    while(1)
+    {
+        if ( motor_is_unsafe )
+        {
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        while( xQueueReceive(user_speed_cmd_2_motor_ctrl_queue, &speed_data, 0) == pdFALSE && xQueueReceive(heat_ctrl_2_motor_ctrl_queue, &heat_data, 0) == pdFALSE )
+        {
+            vTaskDelay(5 / portTICK_PERIOD_MS);
+        }
+
+        speed_data.final_ratio = speed_data.ratio * heat_data.ratio * _speed_limite_ratio;
+        speed_data.speed = speed_data.final_ratio * DAC_MAX_VALUE;
+        if ( speed_data.speed > DAC_MAX_VALUE )
+            speed_data.speed = DAC_MAX_VALUE;
+        if ( speed_data.speed < 0 )
+            speed_data.speed = 0;
+        dacWrite(ADC_MOTOR_PIN, speed_data.speed);
+        
+        xQueueOverwrite(motor_ctrl_queue_cmd_2_communication_queue, &speed_data);
+
+        vTaskDelay(20 / portTICK_PERIOD_MS);
+    }
+}
 
 //get the speed from the user, scale it to a ratio and send it to motor_control_task
 void user_speed_command_task(void *pvParameters)
 {    
-    Speed_Data speed_Data;
+    Speed_Data speed_data;
     uint16_t VIN;
     float speed;
     percent_t ratio;
@@ -58,46 +88,46 @@ void user_speed_command_task(void *pvParameters)
         VIN = analogRead(ADC_PEDAL_PIN);
         xSemaphoreGive(ADC_mutex);
 
-        speed_Data.adc.raw = VIN;
-        speed_Data.adc.voltage = (VIN*ADC_VOLTAGE)/ADC_MAX_VALUE;
+        speed_data.adc.raw = VIN;
+        speed_data.adc.voltage = (VIN*ADC_VOLTAGE)/ADC_MAX_VALUE;
         FilterPush(&_speed_filter, VIN);
-        speed_Data.adc.filtered = FilterDoubledMeanFloat(&_speed_filter, 1.2f);
+        speed_data.adc.filtered = FilterDoubledMeanFloat(&_speed_filter, 1.2f);
 
         if ( VIN < SPEED_NO_SIGNAL )
         {
-            speed_Data.adc.no_signal_count++;
+            speed_data.adc.no_signal_count++;
         }
         else
         {
-            if ( speed_Data.adc.no_signal_count > 0)
-              speed_Data.adc.no_signal_count--;
+            if ( speed_data.adc.no_signal_count > 0)
+              speed_data.adc.no_signal_count--;
         }
 
-        if( speed_Data.adc.no_signal_count > SAMPLES_SIZE )
+        if( speed_data.adc.no_signal_count > SAMPLES_SIZE )
         {
-            ErrorNoSignal(speed_Data);
+            ErrorNoSignal(speed_data);
             vTaskDelay(1 / portTICK_PERIOD_MS);
         }
         else
         {
-            speed = PEDAL_CORRECTION_FACTOR * speed_Data.adc.filtered;
+            speed = PEDAL_CORRECTION_FACTOR * speed_data.adc.filtered;
             if ( speed > SPEED_SLEEP_SIGNAL)
             {
                 ratio = (speed - SPEED_SLEEP_SIGNAL)/(SPEED_MAX_SIGNAL - SPEED_SLEEP_SIGNAL);
                 if(ratio > 1.0f) ratio = 1.0f;
-                speed_Data.ratio = ratio;
+                speed_data.ratio = ratio;
             }
             else
             {
-                speed_Data.ratio = 0.0f;
+                speed_data.ratio = 0.0f;
             }
-            xQueueOverwrite(user_speed_cmd_2_motor_ctrl_queue, &speed_Data);
+            xQueueOverwrite(user_speed_cmd_2_motor_ctrl_queue, &speed_data);
         }
         vTaskDelay(5 / portTICK_PERIOD_MS);
     }
 }
 
-void ErrorNoSignal(Speed_Data speed_data)
+static void ErrorNoSignal(Speed_Data speed_data)
 {
     motor_is_unsafe = true;
     if ( speed_data.adc.no_signal_count < 2*SAMPLES_SIZE )
