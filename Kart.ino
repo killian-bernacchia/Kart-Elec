dbabfada82959c9d34d1ab4d399af214c028a7b4
killian-bernacchia <etude.killbern@gmail.com>
2025-02-03 02:57:18 +0100

    put everything together

--- Kart.ino
+++ Kart.ino
@@ -5,17 +5,36 @@
 
 SemaphoreHandle_t ADC_mutex;
 
-void setup() {
-    //motor_control_init(1.0f);
-    //heat_control_init();
+TaskHandle_t heat_control_tsk_hndl; // get the temperature
+TaskHandle_t user_speed_command_tsk_hndl; // get the speed
+TaskHandle_t motor_control_tsk_hndl; // send the speed
+//TaskHandle_t communication_tsk_hndl; // send the data
+//TaskHandle_t motor_speed_mode_tsk_hndl; // change motor mode
 
-    //xTaskCreate(speed_command_task, "Speed command task", 1000, NULL, 1, NULL);
-    //xTaskCreate(heat_control_task, "Heat control task", 1000, NULL, 1, NULL);
-    //xTaskCreate(motor_control_task, "Motor control task", 1000, NULL, 1, NULL); 
-    //xTaskCreate(communication_task, "Communication task", 1000, NULL, 1, NULL);
+void setup() {
+
+    Serial.begin(115200);
+    pinMode(ADC_MOTOR_PIN, OUTPUT);
+    pinMode(ADC_PEDAL_PIN, INPUT);
+    pinMode(ADC_HEAT_PIN, INPUT);
+    dacWrite(ADC_MOTOR_PIN, SPEED_SLEEP_SIGNAL);     //repos = 0? ou 800mv? // 62 = environ 800mV
+
+    //------------Init Lcd-----------/
+    //lcd.init();
+    //lcd.init();
+    //lcd.backlight();
+    //lcd.setCursor(6, 0);
+    //lcd.print("eKart");
+
+    ADC_mutex = xSemaphoreCreateMutex();
+    motor_control_init(1.0f);
+    heat_control_init();
+
+    xTaskCreatePinnedToCore(heat_control_task, "Heat control task", 2048, NULL, 1, &heat_control_tsk_hndl, 1);
+    xTaskCreatePinnedToCore(user_speed_command_task, "User speed command task", 2048, NULL, 1, &user_speed_command_tsk_hndl, 1);
+    xTaskCreatePinnedToCore(motor_control_task, "Motor control task", 2048, NULL, 2, &motor_control_tsk_hndl, 1);
+    //xTaskCreate(communication_task, "Communication task", 4096, NULL, 1, communication_tsk_hndl, 0);
     return;
 }
 
-void loop() {
-  return;
-}
+void loop(){;}
